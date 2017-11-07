EOSIO/eos

Renamed eos namespace to eosio (STAT-136)

contracts/currency/currency.hpp
@@ -35,7 +35,7 @@ namespace TOKEN_NAME {
    /**
    * Defines a currency token
    */
-   typedef eos::token<uint64_t,N(currency)> CurrencyTokens;
+   typedef eosio::token<uint64_t,N(currency)> CurrencyTokens;
 
    /**
     *  Transfer requires that the sender and receiver be the first two

contracts/dice/dice.hpp
@@ -6,7 +6,7 @@
 namespace dice {
 
    struct OfferBet {
-      eos::Tokens   amount;
+      eosio::Tokens   amount;
       AccountName   player;
       uint256       commitment;
    };
@@ -24,7 +24,7 @@ namespace dice {
       uint64_t gameid = 0;
    };
 
-   using EosTokens = eos::Tokens;
+   using EosTokens = eosio::Tokens;
 
    struct OfferPrimaryKey {
       EosTokens          bet;

contracts/exchange/exchange.cpp
@@ -38,7 +38,7 @@
 #include <eoslib/print.hpp>
 
 using namespace exchange;
-using namespace eos;
+using namespace eosio;
 
 namespace exchange {
 inline void save( const Account& a ) {
@@ -83,7 +83,7 @@ void apply_currency_transfer( const currency::Transfer& transfer ) {
  *  This method is called after the "transfer" action of code
  *  "currencya" is called and "exchange" is listed in the notifiers.
  */
-void apply_eos_transfer( const eos::Transfer& transfer ) {
+void apply_eos_transfer( const eosio::Transfer& transfer ) {
    if( transfer.to == N(exchange) ) {
       modifyAccount( transfer.from, [&]( Account& account ){ 
           account.eos_balance += transfer.quantity; 
@@ -102,9 +102,9 @@ void apply_eos_transfer( const eos::Transfer& transfer ) {
 void match( Bid& bid, Account& buyer, Ask& ask, Account& seller ) {
    print( "match bid: ", bid, "\nmatch ask: ", ask, "\n");
 
-   eos::Tokens ask_eos = ask.quantity * ask.price;
+   eosio::Tokens ask_eos = ask.quantity * ask.price;
 
-   EosTokens      fill_amount_eos = min<eos::Tokens>( ask_eos, bid.quantity );
+   EosTokens      fill_amount_eos = min<eosio::Tokens>( ask_eos, bid.quantity );
    CurrencyTokens fill_amount_currency;
 
    if( fill_amount_eos == ask_eos ) { /// complete fill of ask
@@ -132,7 +132,7 @@ void apply_exchange_buy( BuyOrder order ) {
    Bid& bid = order;
    require_auth( bid.buyer.name );
 
-   assert( bid.quantity > eos::Tokens(0), "invalid quantity" );
+   assert( bid.quantity > eosio::Tokens(0), "invalid quantity" );
    assert( bid.expiration > now(), "order expired" );
 
    print( Name(bid.buyer.name), " created bid for ", order.quantity, " currency at price: ", order.price, "\n" );
@@ -324,7 +324,7 @@ extern "C" {
       } 
       else if( code == N(eos) ) {
         if( action == N(transfer) ) 
-           apply_eos_transfer( current_message<eos::Transfer>() );
+           apply_eos_transfer( current_message<eosio::Transfer>() );
       } 
       else {
       }

contracts/storage/storage.cpp
@@ -14,8 +14,8 @@ namespace TOKEN_NAME {
    }
 
    void apply_storage_transfer( const TOKEN_NAME::Transfer& transfer ) {
-      eos::require_notice( transfer.to, transfer.from );
-      eos::require_auth( transfer.from );
+      eosio::require_notice( transfer.to, transfer.from );
+      eosio::require_auth( transfer.from );
 
       Account from = getAccount( transfer.from );
       Account to   = getAccount( transfer.to );
@@ -50,8 +50,8 @@ namespace TOKEN_NAME {
       char tmp[4098];
       auto bufferlen = read_message(tmp, 4098);
       auto linklen = readLinkFromBuffer( tmp, bufferlen, link, eospathlen, ipfspathlen );
-      eos::require_notice( link.owner );
-      eos::require_auth( link.owner );
+      eosio::require_notice( link.owner );
+      eosio::require_auth( link.owner );
       validate_ipfspath( link.ipfspath, ipfspathlen );
       validate_eospath( link.eospath, eospathlen );
       ::store_str( current_code(), N(storage), link.eospath, eospathlen, (char*)&link, linklen );
@@ -63,7 +63,7 @@ namespace TOKEN_NAME {
       TOKEN_NAME::Link link;
       uint32_t ipfspathlen;
       len = readLinkFromBuffer( tmp, len, link, eospathlen, ipfspathlen );
-      eos::require_auth( link.owner );
+      eosio::require_auth( link.owner );
       uint32_t stake = link.stake;
       ::remove_str( current_code(), N(storage), link.eospath, eospathlen );
       // Reduce Quota usage in Account table
@@ -77,7 +77,7 @@ namespace TOKEN_NAME {
       uint32_t ipfspathlen;
       len = readLinkFromBuffer( tmp, len, link, eospathlen, ipfspathlen );
       
-      // eos::require_auth( producer )
+      // eosio::require_auth( producer )
       // How do we validate the require_auth() is a producer?
       // logic goes here to reduce number of tokens and increase quote used using bancor algorithm
       link.accept = 1;
@@ -90,7 +90,7 @@ namespace TOKEN_NAME {
       TOKEN_NAME::Link link;
       uint32_t ipfspathlen;
       len = readLinkFromBuffer( tmp, len, link, eospathlen, ipfspathlen );
-      // eos::require_auth( producer )
+      // eosio::require_auth( producer )
       // How do we validate the require_auth() is a producer?
       link.accept = 0;
       ::store_str( current_code(), N(storage), link.eospath, eospathlen, (char*)&link, len );
@@ -109,7 +109,7 @@ extern "C" {
     void apply( uint64_t code, uint64_t action ) {
        if( code == N(storage) ) {
           if( action == N(transfer) ) {
-               TOKEN_NAME::apply_storage_transfer( eos::current_message< TOKEN_NAME::Transfer >() );
+               TOKEN_NAME::apply_storage_transfer( eosio::current_message< TOKEN_NAME::Transfer >() );
           } else if (action == N(setlink) ) {
                TOKEN_NAME::apply_storage_setlink(); 
           } else if (action == N(removelink) ) {

openchatproject/safeos

contracts/currency/currency.hpp
@@ -35,7 +35,7 @@ namespace TOKEN_NAME {
    /**
    * Defines a currency token
    */
-   typedef eos::token<uint64_t,N(currency)> currency_tokens;
+   typedef eosio::token<uint64_t,N(currency)> currency_tokens;
 
    /**
     *  transfer requires that the sender and receiver be the first two

contracts/dice/dice.hpp
@@ -6,7 +6,7 @@
 namespace dice {
 
    struct offer_bet {
-      eos::tokens   amount;
+      eosio::tokens   amount;
       account_name  player;
       uint256       commitment;
    };
@@ -24,7 +24,7 @@ namespace dice {
       uint64_t gameid = 0;
    };
 
-   using eos_tokens = eos::tokens;
+   using eos_tokens = eosio::tokens;
 
    struct offer_primary_key {
       eos_tokens          bet;

contracts/exchange/exchange.cpp
@@ -38,7 +38,7 @@
 #include <eoslib/print.hpp>
 
 using namespace exchange;
-using namespace eos;
+using namespace eosio;
 
 namespace exchange {
 inline void save( const account& a ) {
@@ -83,7 +83,7 @@ void apply_currency_transfer( const currency::transfer& transfer ) {
  *  This method is called after the "transfer" action of code
  *  "currencya" is called and "exchange" is listed in the notifiers.
  */
-void apply_eos_transfer( const eos::transfer& transfer ) {
+void apply_eos_transfer( const eosio::transfer& transfer ) {
    if( transfer.to == N(exchange) ) {
       modify_account( transfer.from, [&]( account& mod_account ){
          mod_account.eos_balance += transfer.quantity;
@@ -102,9 +102,9 @@ void apply_eos_transfer( const eos::transfer& transfer ) {
 void match( bid& bid_to_match, account& buyer, ask& ask_to_match, account& seller ) {
    print( "match bid: ", bid_to_match, "\nmatch ask: ", ask_to_match, "\n");
 
-   eos::tokens ask_eos = ask_to_match.quantity * ask_to_match.at_price;
+   eosio::tokens ask_eos = ask_to_match.quantity * ask_to_match.at_price;
 
-   eos_tokens      fill_amount_eos = min<eos::tokens>( ask_eos, bid_to_match.quantity );
+   eos_tokens      fill_amount_eos = min<eosio::tokens>( ask_eos, bid_to_match.quantity );
    currency_tokens fill_amount_currency;
 
    if( fill_amount_eos == ask_eos ) { /// complete fill of ask_to_match
@@ -132,7 +132,7 @@ void apply_exchange_buy( buy_order order ) {
    bid& exchange_bid = order;
    require_auth( exchange_bid.buyer.name );
 
-   assert( exchange_bid.quantity > eos::tokens(0), "invalid quantity" );
+   assert( exchange_bid.quantity > eosio::tokens(0), "invalid quantity" );
    assert( exchange_bid.expiration > now(), "order expired" );
 
    print( Name(exchange_bid.buyer.name), " created bid for ", order.quantity, " currency at price: ", order.at_price, "\n" );
@@ -324,7 +324,7 @@ extern "C" {
       } 
       else if( code == N(eos) ) {
         if( action == N(transfer) ) 
-           apply_eos_transfer( current_message<eos::transfer>() );
+           apply_eos_transfer( current_message<eosio::transfer>() );
       } 
       else {
       }

contracts/storage/storage.cpp
@@ -14,8 +14,8 @@ namespace TOKEN_NAME {
    }
 
    void apply_storage_transfer( const TOKEN_NAME::transfer& transfer_storage ) {
-      eos::require_notice( transfer_storage.to, transfer_storage.from );
-      eos::require_auth( transfer_storage.from );
+      eosio::require_notice( transfer_storage.to, transfer_storage.from );
+      eosio::require_auth( transfer_storage.from );
 
       account from = get_account( transfer_storage.from );
       account to   = get_account( transfer_storage.to );
@@ -50,8 +50,8 @@ namespace TOKEN_NAME {
       char tmp[4098];
       auto bufferlen = read_message(tmp, 4098);
       auto linklen = read_link_from_buffer( tmp, bufferlen, link_to_set, eospathlen, ipfspathlen );
-      eos::require_notice( link_to_set.owner );
-      eos::require_auth( link_to_set.owner );
+      eosio::require_notice( link_to_set.owner );
+      eosio::require_auth( link_to_set.owner );
       validate_ipfspath( link_to_set.ipfspath, ipfspathlen );
       validate_eospath( link_to_set.eospath, eospathlen );
       ::store_str( current_code(), N(storage), link_to_set.eospath, eospathlen, (char*)&link_to_set, linklen );
@@ -63,7 +63,7 @@ namespace TOKEN_NAME {
       TOKEN_NAME::link link_to_remove;
       uint32_t ipfspathlen;
       len = read_link_from_buffer( tmp, len, link_to_remove, eospathlen, ipfspathlen );
-      eos::require_auth( link_to_remove.owner );
+      eosio::require_auth( link_to_remove.owner );
       uint32_t stake = link_to_remove.stake;
       ::remove_str( current_code(), N(storage), link_to_remove.eospath, eospathlen );
       // Reduce Quota usage in account table
@@ -77,7 +77,7 @@ namespace TOKEN_NAME {
       uint32_t ipfspathlen;
       len = read_link_from_buffer( tmp, len, link_to_create, eospathlen, ipfspathlen );
       
-      // eos::require_auth( producer )
+      // eosio::require_auth( producer )
       // How do we validate the require_auth() is a producer?
       // logic goes here to reduce number of tokens and increase quote used using bancor algorithm
       link_to_create.accept = 1;
@@ -90,7 +90,7 @@ namespace TOKEN_NAME {
       TOKEN_NAME::link link_to_reject;
       uint32_t ipfspathlen;
       len = read_link_from_buffer( tmp, len, link_to_reject, eospathlen, ipfspathlen );
-      // eos::require_auth( producer )
+      // eosio::require_auth( producer )
       // How do we validate the require_auth() is a producer?
       link_to_reject.accept = 0;
       ::store_str( current_code(), N(storage), link_to_reject.eospath, eospathlen, (char*)&link_to_reject, len );
@@ -109,7 +109,7 @@ extern "C" {
     void apply( uint64_t code, uint64_t action ) {
        if( code == N(storage) ) {
           if( action == N(transfer) ) {
-               TOKEN_NAME::apply_storage_transfer( eos::current_message< TOKEN_NAME::transfer >() );
+               TOKEN_NAME::apply_storage_transfer( eosio::current_message< TOKEN_NAME::transfer >() );
           } else if (action == N(setlink) ) {
                TOKEN_NAME::apply_storage_setlink(); 
           } else if (action == N(removelink) ) {