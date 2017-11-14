sinanlin/eos

Authenticate peers during network handshake.
Authenticates producers using producer keys, optionally authenticates
non-producing peers using configured keys, and further optionally
allows unauthenticated connections.  Includes launcher support.

Also fix bug when a 100% producer node started up after a long (>64
second) delay and thought it's a 0% node.

Add additional information for debugging Travis builds.

libraries/chain/include/eos/chain/config.hpp
@@ -34,6 +34,8 @@ const static int BlockIntervalSeconds = 3;
 const static int Percent100 = 10000;
 const static int Percent1 = 100;
 
+const static UInt32 RequiredProducerParticipation = 33 * config::Percent1;
+
 const static UInt32 DefaultMaxBlockSize = 5 * 1024 * 1024;
 const static UInt32 DefaultTargetBlockSize = 128 * 1024;
 const static UInt64 DefaultMaxStorageSize = 10 * 1024;

programs/launcher/main.cpp
@@ -218,11 +218,19 @@ enum launch_modes {
   LM_ALL
 };
 
+enum allowed_connection : char {
+  PC_NONE = 0,
+  PC_PRODUCERS = 1 << 0,
+  PC_SPECIFIED = 1 << 1,
+  PC_ANY = 1 << 2
+};
+
 struct launcher_def {
-  int producers;
-  int total_nodes;
-  int prod_nodes;
+  size_t producers;
+  size_t total_nodes;
+  size_t prod_nodes;
   string shape;
+  allowed_connection allowed_connections = PC_NONE;
   bf::path genesis;
   bf::path output;
   bool skip_transaction_signatures = false;
@@ -254,33 +262,42 @@ struct launcher_def {
 void
 launcher_def::set_options (bpo::options_description &cli) {
   cli.add_options()
-    ("nodes,n",bpo::value<int>()->default_value(1),"total number of nodes to configure and launch")
-    ("pnodes,p",bpo::value<int>()->default_value(1),"number of nodes that are producers")
-    ("shape,s",bpo::value<string>()->default_value("ring"),"network topology, use \"ring\" \"star\" \"mesh\" or give a filename for custom")
-    ("genesis,g",bpo::value<bf::path>()->default_value("./genesis.json"),"set the path to genesis.json")
-    ("output,o",bpo::value<bf::path>(),"save a copy of the generated topology in this file")
-    ("skip-signature", bpo::bool_switch()->default_value(false), "EOSD does not require transaction signatures.")
-    ("eosd", bpo::value<string>(), "forward eosd command line argument(s) to each instance of eosd, enclose arg in quotes")
+    ("nodes,n",bpo::value<size_t>(&total_nodes)->default_value(1),"total number of nodes to configure and launch")
+    ("pnodes,p",bpo::value<size_t>(&prod_nodes)->default_value(1),"number of nodes that are producers")
+    ("mode,m",bpo::value<vector<string>>()->multitoken()->default_value({"any"}, "any"),"connection mode, combination of \"any\", \"producers\", \"specified\", \"none\"")
+    ("shape,s",bpo::value<string>(&shape)->default_value("ring"),"network topology, use \"ring\" \"star\" \"mesh\" or give a filename for custom")
+    ("genesis,g",bpo::value<bf::path>(&genesis)->default_value("./genesis.json"),"set the path to genesis.json")
+    ("output,o",bpo::value<bf::path>(&output),"save a copy of the generated topology in this file")
+    ("skip-signature", bpo::bool_switch(&skip_transaction_signatures)->default_value(false), "EOSD does not require transaction signatures.")
+    ("eosd", bpo::value<string>(&eosd_extra_args), "forward eosd command line argument(s) to each instance of eosd, enclose arg in quotes")
         ;
+}
+
+template<class enum_type, class=typename std::enable_if<std::is_enum<enum_type>::value>::type>
+inline enum_type& operator|=(enum_type&lhs, const enum_type& rhs)
+{
+  using T = std::underlying_type_t <enum_type>;
+  return lhs = static_cast<enum_type>(static_cast<T>(lhs) | static_cast<T>(rhs));
 }
 
 void
 launcher_def::initialize (const variables_map &vmap) {
-  if (vmap.count("nodes"))
-    total_nodes = vmap["nodes"].as<int>();
-  if (vmap.count("pnodes"))
-    prod_nodes = vmap["pnodes"].as<int>();
-  if (vmap.count("shape"))
-    shape = vmap["shape"].as<string>();
-  if (vmap.count("genesis"))
-    genesis = vmap["genesis"].as<bf::path>();
-  if (vmap.count("output"))
-    output = vmap["output"].as<bf::path>();
-  if (vmap.count("skip-signature"))
-    skip_transaction_signatures = vmap["skip-signature"].as<bool>();
-  if (vmap.count("eosd"))
-    eosd_extra_args = vmap["eosd"].as<string>();
-
+  if (vmap.count("mode")) {
+    const vector<string> modes = vmap["mode"].as<vector<string>>();
+    for(const string&m : modes)
+    {
+      if (boost::iequals(m, "any"))
+        allowed_connections |= PC_ANY;
+      else if (boost::iequals(m, "producers"))
+        allowed_connections |= PC_PRODUCERS;
+      else if (boost::iequals(m, "specified"))
+        allowed_connections |= PC_SPECIFIED;
+      else {
+        cerr << "unrecognized connection mode: " << m << endl;
+        exit (-1);
+      }
+    }
+  }
   producers = 21;
   data_dir_base = "tn_data_";
   alias_base = "testnet_";
@@ -341,7 +358,7 @@ void
 launcher_def::define_nodes () {
   int per_node = producers / prod_nodes;
   int extra = producers % prod_nodes;
-  for (int i = 0; i < total_nodes; i++) {
+  for (size_t i = 0; i < total_nodes; i++) {
     eosd_def node;
     string dex = boost::lexical_cast<string,int>(i);
     string name = alias_base + dex;
@@ -413,6 +430,21 @@ launcher_def::write_config_file (eosd_def &node) {
       << "http-server-endpoint = " << node.hostname << ":" << node.http_port << "\n"
       << "listen-endpoint = 0.0.0.0:" << node.p2p_port << "\n"
       << "public-endpoint = " << node.public_name << ":" << node.p2p_port << "\n";
+  if (allowed_connections & PC_ANY) {
+    cfg << "allowed-connection = any\n";
+  }
+  else
+  {
+    if (allowed_connections & PC_PRODUCERS) {
+      cfg << "allowed-connection = producers\n";
+    }
+    if (allowed_connections & PC_SPECIFIED) {
+      cfg << "allowed-connection = specified\n";
+      cfg << "peer-key = \"" << node.keys.begin()->public_key << "\"\n";
+      cfg << "peer-private-key = [\"" << node.keys.begin()->public_key
+          << "\",\"" << node.keys.begin()->wif_private_key << "\"]\n";
+    }
+  }
   for (const auto &p : node.peers) {
     cfg << "remote-endpoint = " << network.nodes.find(p)->second.p2p_endpoint() << "\n";
   }
@@ -729,21 +761,18 @@ int main (int argc, char *argv[]) {
   top.set_options(opts);
 
   opts.add_options()
-    ("timestamp,i",bpo::value<string>(),"set the timestamp for the first block. Use \"now\" to indicate the current time")
+    ("timestamp,i",bpo::value<string>(&gts),"set the timestamp for the first block. Use \"now\" to indicate the current time")
     ("launch,l",bpo::value<string>(), "select a subset of nodes to launch. Currently may be \"all\", \"none\", or \"local\". If not set, the default is to launch all unless an output file is named, in which case it starts none.")
-    ("kill,k", bpo::value<string>(),"The launcher retrieves the previously started process ids and issue a kill signal to each.")
+    ("kill,k", bpo::value<string>(&kill_arg),"The launcher retrieves the previously started process ids and issue a kill signal to each.")
     ("help,h","print this list");
 
 
   try {
     bpo::store(bpo::parse_command_line(argc, argv, opts), vmap);
+    bpo::notify(vmap);
 
     top.initialize(vmap);
 
-    if (vmap.count("timestamp"))
-      gts = vmap["timestamp"].as<string>();
-    if (vmap.count("kill"))
-      kill_arg = vmap["kill"].as<string>();
     if (vmap.count("help") > 0) {
       opts.print(cerr);
       return 0;

openchatproject/safeos

libraries/chain/include/eos/chain/config.hpp
@@ -39,6 +39,8 @@ const static int default_per_auth_account = 1800;
 const static int default_per_code_account_time_frame_seconds = 18;
 const static int default_per_code_account = 18000;
 
+const static uint32 required_producer_participation = 33 * config::percent1;
+
 const static uint32 default_max_block_size = 5 * 1024 * 1024;
 const static uint32 default_target_block_size = 128 * 1024;
 const static uint64 default_max_storage_size = 10 * 1024;

programs/launcher/main.cpp
@@ -220,11 +220,19 @@ enum launch_modes {
   LM_ALL
 };
 
+enum allowed_connection : char {
+  PC_NONE = 0,
+  PC_PRODUCERS = 1 << 0,
+  PC_SPECIFIED = 1 << 1,
+  PC_ANY = 1 << 2
+};
+
 struct launcher_def {
-  int producers;
-  int total_nodes;
-  int prod_nodes;
+  size_t producers;
+  size_t total_nodes;
+  size_t prod_nodes;
   string shape;
+  allowed_connection allowed_connections = PC_NONE;
   bf::path genesis;
   bf::path output;
   bool skip_transaction_signatures = false;
@@ -257,36 +265,43 @@ struct launcher_def {
 void
 launcher_def::set_options (bpo::options_description &cli) {
   cli.add_options()
-    ("nodes,n",bpo::value<int>()->default_value(1),"total number of nodes to configure and launch")
-    ("pnodes,p",bpo::value<int>()->default_value(1),"number of nodes that are producers")
-    ("shape,s",bpo::value<string>()->default_value("star"),"network topology, use \"ring\" \"star\" \"mesh\" or give a filename for custom")
-    ("genesis,g",bpo::value<bf::path>()->default_value("./genesis.json"),"set the path to genesis.json")
-    ("output,o",bpo::value<bf::path>(),"save a copy of the generated topology in this file")
-    ("skip-signature", bpo::bool_switch()->default_value(false), "EOSD does not require transaction signatures.")
-    ("eosd", bpo::value<string>(), "forward eosd command line argument(s) to each instance of eosd, enclose arg in quotes")
-    ("delay,d",bpo::value<int>()->default_value(0),"seconds delay before starting each node after the first")
+    ("nodes,n",bpo::value<size_t>(&total_nodes)->default_value(1),"total number of nodes to configure and launch")
+    ("pnodes,p",bpo::value<size_t>(&prod_nodes)->default_value(1),"number of nodes that are producers")
+    ("mode,m",bpo::value<vector<string>>()->multitoken()->default_value({"any"}, "any"),"connection mode, combination of \"any\", \"producers\", \"specified\", \"none\"")
+    ("shape,s",bpo::value<string>(&shape)->default_value("star"),"network topology, use \"ring\" \"star\" \"mesh\" or give a filename for custom")
+    ("genesis,g",bpo::value<bf::path>(&genesis)->default_value("./genesis.json"),"set the path to genesis.json")
+    ("output,o",bpo::value<bf::path>(&output),"save a copy of the generated topology in this file")
+    ("skip-signature", bpo::bool_switch(&skip_transaction_signatures)->default_value(false), "EOSD does not require transaction signatures.")
+    ("eosd", bpo::value<string>(&eosd_extra_args), "forward eosd command line argument(s) to each instance of eosd, enclose arg in quotes")
+    ("delay,d",bpo::value<int>(&start_delay)->default_value(0),"seconds delay before starting each node after the first")
         ;
+}
+
+template<class enum_type, class=typename std::enable_if<std::is_enum<enum_type>::value>::type>
+inline enum_type& operator|=(enum_type&lhs, const enum_type& rhs)
+{
+  using T = std::underlying_type_t <enum_type>;
+  return lhs = static_cast<enum_type>(static_cast<T>(lhs) | static_cast<T>(rhs));
 }
 
 void
 launcher_def::initialize (const variables_map &vmap) {
-  if (vmap.count("nodes"))
-    total_nodes = vmap["nodes"].as<int>();
-  if (vmap.count("pnodes"))
-    prod_nodes = vmap["pnodes"].as<int>();
-  if (vmap.count("shape"))
-    shape = vmap["shape"].as<string>();
-  if (vmap.count("genesis"))
-    genesis = vmap["genesis"].as<bf::path>();
-  if (vmap.count("output"))
-    output = vmap["output"].as<bf::path>();
-  if (vmap.count("skip-signature"))
-    skip_transaction_signatures = vmap["skip-signature"].as<bool>();
-  if (vmap.count("eosd"))
-    eosd_extra_args = vmap["eosd"].as<string>();
-  if (vmap.count("delay"))
-    start_delay = vmap["delay"].as<int>();
-
+  if (vmap.count("mode")) {
+    const vector<string> modes = vmap["mode"].as<vector<string>>();
+    for(const string&m : modes)
+    {
+      if (boost::iequals(m, "any"))
+        allowed_connections |= PC_ANY;
+      else if (boost::iequals(m, "producers"))
+        allowed_connections |= PC_PRODUCERS;
+      else if (boost::iequals(m, "specified"))
+        allowed_connections |= PC_SPECIFIED;
+      else {
+        cerr << "unrecognized connection mode: " << m << endl;
+        exit (-1);
+      }
+    }
+  }
   producers = 21;
   data_dir_base = "tn_data_";
   alias_base = "testnet_";
@@ -359,7 +374,7 @@ void
 launcher_def::define_nodes () {
   int per_node = producers / prod_nodes;
   int extra = producers % prod_nodes;
-  for (int i = 0; i < total_nodes; i++) {
+  for (size_t i = 0; i < total_nodes; i++) {
     eosd_def node;
     string dex = boost::lexical_cast<string,int>(i);
     string name = alias_base + dex;
@@ -432,6 +447,21 @@ launcher_def::write_config_file (eosd_def &node) {
       << "http-server-endpoint = " << node.hostname << ":" << node.http_port << "\n"
       << "listen-endpoint = 0.0.0.0:" << node.p2p_port << "\n"
       << "public-endpoint = " << node.public_name << ":" << node.p2p_port << "\n";
+  if (allowed_connections & PC_ANY) {
+    cfg << "allowed-connection = any\n";
+  }
+  else
+  {
+    if (allowed_connections & PC_PRODUCERS) {
+      cfg << "allowed-connection = producers\n";
+    }
+    if (allowed_connections & PC_SPECIFIED) {
+      cfg << "allowed-connection = specified\n";
+      cfg << "peer-key = \"" << node.keys.begin()->public_key << "\"\n";
+      cfg << "peer-private-key = [\"" << node.keys.begin()->public_key
+          << "\",\"" << node.keys.begin()->wif_private_key << "\"]\n";
+    }
+  }
   for (const auto &p : node.peers) {
     cfg << "remote-endpoint = " << network.nodes.find(p)->second.p2p_endpoint() << "\n";
   }
@@ -750,23 +780,19 @@ int main (int argc, char *argv[]) {
   top.set_options(opts);
 
   opts.add_options()
-    ("timestamp,i",bpo::value<string>(),"set the timestamp for the first block. Use \"now\" to indicate the current time")
+    ("timestamp,i",bpo::value<string>(&gts),"set the timestamp for the first block. Use \"now\" to indicate the current time")
     ("launch,l",bpo::value<string>(), "select a subset of nodes to launch. Currently may be \"all\", \"none\", or \"local\". If not set, the default is to launch all unless an output file is named, in which case it starts none.")
-    ("kill,k", bpo::value<string>(),"The launcher retrieves the previously started process ids and issue a kill signal to each.")
+    ("kill,k", bpo::value<string>(&kill_arg),"The launcher retrieves the previously started process ids and issue a kill signal to each.")
     ("version,v", "print version information")
     ("help,h","print this list");
 
 
   try {
     bpo::store(bpo::parse_command_line(argc, argv, opts), vmap);
+    bpo::notify(vmap);
 
     top.initialize(vmap);
 
-    if (vmap.count("timestamp"))
-      gts = vmap["timestamp"].as<string>();
-    if (vmap.count("kill")) {
-      kill_arg = vmap["kill"].as<string>();
-    }
     if (vmap.count("help") > 0) {
       opts.print(cerr);
       return 0;