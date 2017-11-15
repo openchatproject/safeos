tpinckard/eos

* Change the strategy to export types/typedefs from C++ to the ABI definition * Remove the "@abi type" annotation support, now typedefs are automatically included when needed * Add ABI annotations to proxy contract types * Fix ABI generation tests * Fix travis call to tests * Fix exchange typedef declaration
Ref #535

contracts/proxy/proxy.hpp
@@ -6,6 +6,13 @@
 #include <eoslib/db.hpp>
 
 namespace proxy {
+   
+   //@abi proxy action
+   struct PACKED( SetOwner ) {
+      AccountName owner;	
+   };
+
+   //@abi proxy table
    struct PACKED( Config ) {
       Config( AccountName o = AccountName() ):owner(o){}
       const uint64_t     key = N(config);

openchatproject/safeos

contracts/proxy/proxy.hpp
@@ -6,10 +6,17 @@
 #include <eoslib/db.hpp>
 
 namespace proxy {
+   
+   //@abi proxy action
+   struct PACKED( set_owner ) {
+      account_name owner;	
+   };
+
+   //@abi proxy table
    struct PACKED( config ) {
       config( account_name o = account_name() ):owner(o){}
-      const uint64_t       key = N(config);
-      account_name         owner;
+      const uint64_t     key = N(config);
+      account_name        owner;
    };
 
    using configs = table<N(proxy),N(proxy),N(configs),config,uint64_t>;